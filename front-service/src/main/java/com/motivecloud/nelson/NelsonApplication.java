9c287f21ba289b8760c938525d7259b3124c6099
Pawel Nowicki <paweltechie@gmail.com>
2017-01-19 16:33:31 -0500

    Initial checkin

---
+++ front-service/src/main/java/com/motivecloud/nelson/NelsonApplication.java
@@ -0,0 +1,12 @@
+package com.motivecloud.nelson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NelsonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NelsonApplication.class, args);
+	}
+}
