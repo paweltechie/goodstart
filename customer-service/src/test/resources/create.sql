40258e50b26a66b87e0a5b1e63b22d2cc7bbf83d
Pawel Nowicki <paweltechie@gmail.com>
2017-02-09 19:55:40 -0500

    Adding sql inner queries

---
+++ customer-service/src/test/resources/create.sql
@@ -0,0 +1,12 @@
+CREATE TABLE IF NOT EXISTS nelson.customer (
+  id VARCHAR(45) NOT NULL,
+  first_name VARCHAR(45) NOT NULL,
+  last_name VARCHAR(45) NOT NULL,
+  street_1 VARCHAR(45) NULL,
+  street_2 VARCHAR(45) NULL,
+  city VARCHAR(45) NULL,
+  state VARCHAR(45) NOT NULL,
+  sip VARCHAR(45) NULL,
+  country VARCHAR(45) NOT NULL,
+  email VARCHAR(45) NOT NULL,
+  PRIMARY KEY (id));
